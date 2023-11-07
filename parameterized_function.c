c39b55e27bbf1468088da81045b14603eca467db
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-07 14:35:12 +0530

    Create parameterized_function.c

---
+++ parameterized_function.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int addition(int a , int b ){ //paramertized function
+	int c = a+b;
+	return c ; //return is a reserved keyword
+}
+int main(){
+int result;
+result = addition(14,5);
+printf("Addition is : %d",result);
+}
