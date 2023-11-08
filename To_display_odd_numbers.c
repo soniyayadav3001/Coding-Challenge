872d5c83688ff1bfcc322ea040cc300aa66817a6
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-08 19:16:05 +0530

    Create To_display_odd_numbers.c

---
+++ To_display_odd_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int count = 0;
+	int number = 1;
+	int n;
+	printf("Enter number of odd number you want : ");
+	scanf("\n%d",&n);
+	while(count<=n){
+		if((number%2)!=0){
+			printf("\n%d",number);
+			count++;
+		}
+		number++;
+	}
+	return 0;
+}
