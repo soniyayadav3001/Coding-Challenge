448fdaeafaf224b7c76e436559da304620a04d1d
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-08 19:17:23 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int a;
+	int b;
+	printf("Enter the value of a : ");
+	scanf("%d",&a);
+	printf("Enter the value of b : ");
+	scanf("%d",&b);
+	int c=a+b;
+	printf("Sum is : %d",c);	
+	return 0;
+}
