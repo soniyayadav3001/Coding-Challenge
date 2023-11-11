6ddb910f53438cf59bdc2148afac84420c1b7480
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-11 14:44:30 +0530

    Create Cube of any number.cpp

---
+++ Cube of any number.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number;
+    cout << "Enter the number here : " << endl;
+    cin >> number;
+    int z;
+    z = number * number * number;
+    cout << "The cube of " << number <<" is "<< z << endl;
+    return 0;
+}
