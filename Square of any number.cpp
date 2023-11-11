b68275496092f3fca214028e75ce8b8b2595263f
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-11 14:45:40 +0530

    Create Square of any number.cpp

---
+++ Square of any number.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number;
+    cout << "Enter the number here : " << endl;
+    cin >> number;
+    int z;
+    z = number * number ;
+    cout << "The square of " << number <<" is "<< z << endl;
+    return 0;
+}
