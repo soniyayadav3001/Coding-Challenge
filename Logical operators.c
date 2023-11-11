247e199965d215b1f06bf7401f09e8336c15a790
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-11 14:51:55 +0530

    Create Logical operators.c

---
+++ Logical operators.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	//Logical operators
+	int a= 10;
+	int b= 20;
+	int c=30;
+	int d;
+	d=((a<b)||(b==c));
+	printf("The result of (a<b)||(b==c) : %d",d );//OR operator
+	d=((a>b)&&(b==c));
+	printf("\n The result of (a>b)&&(b==c) : %d",d );//AND operator
+	d=(!(b==c));
+	printf("\n The result of !(b==c) : %d",d );//NOT operator	
+	
+	return 0;
+}
