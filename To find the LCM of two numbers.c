df544c0e165ef97bcd55c060625c3813dc68757a
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-11 14:00:39 +0530

    Create To find the LCM of two numbers.c

---
+++ To find the LCM of two numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() 
+{
+    int a, b, max;
+    printf("Enter the first number : ");
+    scanf("%d", &a);
+    printf("Enter the second number : ");
+    scanf("%d", &b);
+ 
+    max = (a > b) ? a : b;  // maximum number between a and b is stored in max
+ 
+    while (max % a != 0 || max % b != 0) 
+    {
+            max++;
+    }
+    printf("The LCM of %d and %d is %d.", a, b, max);
+    
+}
