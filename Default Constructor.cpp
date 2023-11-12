dac097ababf46645b86ffe456efaa785a0e33678
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-12 17:07:02 +0530

    Create Default Constructor.cpp

---
+++ Default Constructor.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+class A{
+    public:
+        A(){        //Default constuctor
+            cout << "Hello C++" << endl;
+        }
+
+};
+int main() {
+        A();
+        return 0;
+}
