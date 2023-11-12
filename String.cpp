adb31f01bd11327b6ba6ea422fc3f09074200ad1
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-12 18:17:42 +0530

    Create String.cpp

---
+++ String.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    string s;
+    cout << "Enter the string here : " << endl;
+    cin >> s;
+    cout << "Your string is " << s << endl;
+    return 0;
+}
