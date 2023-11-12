a6eb1e5cea7948063cc29f75670743cbc97a76f9
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-12 17:08:27 +0530

    Create Method Overridding.cpp

---
+++ Method Overridding.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class A{
+    public:
+    void greet(){
+        cout << "Hello" << endl;
+    }
+};
+class B{
+    public:
+    void greet(){
+        cout << "Namaste" << endl;
+    }
+};
+int main() {
+    B obj;
+    obj.greet();
+    A o;
+    o.greet();
+    return 0;
+}
