dde4b0cae31a677347849c4bb4accd64aa6a5322
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-12 18:18:33 +0530

    Create String2.cpp

---
+++ String2.cpp
@@ -0,0 +1,9 @@
+ #include<iostream>
+ using namespace std;
+ int main() {
+     string s;
+     cout << "Enter the string here : " << endl;
+     getline(cin, s);                           //To print full string we use getline function.
+     cout << "Your string is " << s << endl;
+     return 0;
+ }
