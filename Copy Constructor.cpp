df477f19c22f1b340e49dd00653789140d544dd4
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-13 19:26:04 +0530

    Create Copy Constructor.cpp

---
+++ Copy Constructor.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class Student{
+	public:
+		int age;
+		int marks;
+		Student(int a , int m){		//Parameterized Constructor
+			age = a;
+			marks =m;
+		}
+		Student(Student &s){		//Copy Constructor
+			age=s.age;
+			marks=s.marks;
+		}
+		void display(){
+			cout<<" Age is "<<age<<endl;
+			cout<<" Marks is "<<marks<<endl;
+		}
+};
+int main(){
+Student amit(18,100);
+Student ankit(amit);				//Copy Constructor Called
+amit.display();
+ankit.display();
+	return 0;
+}
