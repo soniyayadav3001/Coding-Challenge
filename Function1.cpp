1bb38dd574a127a9cfbf63728620ec2a9499fe97
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-14 19:05:55 +0530

    Create Function1.cpp

---
+++ Function1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int sum(int a , int b) {
+    int c = a + b;
+    cout << " The sum is " << c << endl;
+}
+
+int main() {
+ sum(4, 5);
+ sum(1, 5);
+ sum(3, 4);
+ return 0;
+}
