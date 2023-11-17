b66177f4b2f885b0a8e27199a6c057e9e71f1ccb
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-17 19:40:51 +0530

    Create Convert Decimal to Binary.c

---
+++ Convert Decimal to Binary.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int n, i, a[10];
+  printf("Enter the number to convert: ");
+  scanf("%d", &n);
+  for (i = 0; n > 0; i++) {
+    a[i] = n % 2;
+    n = n / 2;
+  }
+  printf("\nBinary of Given Number is=");
+  for (i = i - 1; i >= 0; i--) {
+    printf("%d", a[i]);
+  }
+  return 0;
+}
