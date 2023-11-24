175d3a285c5fc0fc8112621da264359f9cf3affd
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-24 18:17:48 +0530

    Create Canteen.cpp

---
+++ Canteen.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int order;
+  cout << "*********************WELCOME TO CANTEEN**********************";
+  cout << "\n1. MOMOS    Rs.50";
+  cout << "\n2. DOSA     Rs.85";
+  cout << "\n3. NOODLES  Rs.60";
+  cout << "\n4. SAMOSA   Rs.12";
+  cout << "\n5. COFFEE   Rs.18";
+  cout << "\n6. exit";
+
+  while (order != 6)
+  {
+    cout << "\nEnter your order:";
+    cin >> order;
+    switch (order)
+    {
+    case 1:
+      cout << "Your order is momos....";
+      cout << "\nPlease wait for 2 min";
+      break;
+    case 2:
+      cout << "Your order is dosa....";
+      cout << "\nPlease wait for 2 min";
+      break;
+    case 3:
+      cout << "Your order is noodles....";
+      cout << "\nPlease wait for 2 min";
+      break;
+    case 4:
+      cout << "Your order is samosa....";
+      cout << "\nPlease wait for 2 min";
+      break;
+    case 5:
+      cout << "Your order is coffee....";
+      cout << "\nPlease wait for 2 min";
+      break;
+    default:
+      cout << "thank you ^_^";
+    }
+  }
+  return 0;
+}
