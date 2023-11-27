6d055492d4759eaaf27f26d731b1114d71c75855
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-27 19:00:10 +0530

    Create Data Abstraction.cpp

---
+++ Data Abstraction.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class sum{
+  int num_1, num_2, total;
+  public:
+  int getdata(){
+    cout << "Enter the value of num_1 : ";
+    cin >> num_1;
+    cout << "Enter the value of num_2 : ";
+    cin >> num_2;
+  }
+  int display(){
+    cout << "Value of num_1 is : "<<num_1<<endl;
+    cout << "Value of num_2 is : "<<num_2<<endl;
+  }
+  int add(){
+    total = num_1 + num_2;
+    cout << "Total is " << total;
+  }
+};
+int main() {
+  sum obj;
+  obj.getdata();
+  obj.display();
+  obj.add();
+  return 0;
+}
