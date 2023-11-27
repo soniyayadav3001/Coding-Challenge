ff886604bb26e72ba491e76489171e79a0d876f0
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-27 19:08:33 +0530

    Create To print String vertically.cpp

---
+++ To print String vertically.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string str;
+  cout << "Enter a string: ";
+  cin >> str;
+  
+  for (int i = 0; i < str.length(); i++) {
+    cout << str[i] << endl;
+  }
+
+  return 0;
+}
