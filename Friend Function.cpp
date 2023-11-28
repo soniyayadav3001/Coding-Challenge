126a887b7f81df2b97bad77fd79b46652d13501e
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-28 17:36:58 +0530

    Create Friend Function.cpp

---
+++ Friend Function.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Distance {
+private:
+  int meter;
+
+  // friend function
+  friend int addFive(Distance);
+
+public:
+  Distance(int m) {
+    meter = m;
+  }
+
+  void display() {
+    cout << "Distance: " << meter << " meter" << endl;
+  }
+};
+
+// friend function
+int addFive(Distance d) {
+  return d.meter + 5;
+}
+
+int main() {
+  Distance d1(10);
+
+  // call friend function
+  int newDistance = addFive(d1);
+
+  cout << "Old distance: ";
+  d1.display();
+
+  cout << "New distance after adding 5 meter: ";
+  Distance d2(newDistance);
+  d2.display();
+
+  return 0;
+}
