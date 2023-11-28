70b3dde36913cb9f0cf6fd0647768b4153e41015
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-28 18:21:28 +0530

    Create To check the size of data types.c

---
+++ To check the size of data types.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+  printf("%d\n", sizeof(int));
+  printf("%d\n", sizeof(float));
+  printf("%d\n", sizeof(char));
+  printf("%d\n", sizeof(double));
+  printf("%d\n", sizeof(long double));
+  return 0;
+}
