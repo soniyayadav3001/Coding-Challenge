15feb99435c15092897202279fa75a6746dba5ad
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-28 17:48:33 +0530

    Create Profit Or Loss.cpp

---
+++ Profit Or Loss.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int cp, sp, profit, loss;
+
+  cout << "Enter the cost price: ";
+  cin >> cp;
+  cout << "Enter the selling price: ";
+  cin >> sp;
+
+  if (sp > cp)
+  {
+    profit = sp - cp;
+    cout << "Profit Gained: " << profit;
+  }
+  else if (cp > sp)
+  {
+    loss = cp - sp;
+    cout << "Loss Incurred: " << loss;
+  }
+  else
+  {
+    cout << "Neither profit nor loss" << endl;
+  }
+  return 0;
+}
