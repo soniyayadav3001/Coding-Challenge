d31f2938efe6cbbc9d7bc7420273b0d638b30eeb
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-12-01 21:50:03 +0530

    Create Harshad Number.c

---
+++ Harshad Number.c
@@ -0,0 +1,22 @@
+#include <stdio.h>  
+int main()
+{
+  int num;
+  printf("Enter the number here : ");
+  scanf("%d", &num);
+  int rem = 0, sum = 0, n;
+  n = num;
+
+  while (num > 0)
+  {
+    rem = num % 10;
+    sum = sum + rem;
+    num = num / 10;
+    }
+  
+    if(n%sum == 0)
+        printf("%d is a harshad number", n);
+    else
+        printf("%d is not a harshad number", n);
+    return 0;
+}
