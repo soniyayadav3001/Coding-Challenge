4785f4a407185510d264f1c8880b59c87e8c3701
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-12-02 22:02:36 +0530

    Update Harshad Number.c

--- Harshad Number.c
+++ Harshad Number.c
@@ -6,14 +6,12 @@ int main()
   scanf("%d", &num);
   int rem = 0, sum = 0, n;
   n = num;
-
   while (num > 0)
   {
     rem = num % 10;
     sum = sum + rem;
     num = num / 10;
     }
-  
     if(n%sum == 0)
         printf("%d is a harshad number", n);
     else
