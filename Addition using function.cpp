38e76fd7d2e92b735b72ff16db82bdf8d80d08e0
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-12-03 16:15:17 +0530

    Create Addition using function.cpp

---
+++ Addition using function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int add(int a, int b) {
+  return a + b;
+}
+int main() {
+  int x, y, z;
+
+  cout << "Enter two numbers: ";
+  cin >> x >> y;
+  z = add(x, y);
+  cout << "The sum of " << x << " and " << y << " is " << z << endl;
+
+  return 0;
+}
