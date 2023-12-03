087331436bf15f14a1a38eca3b6a6da97cdf8f85
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-12-03 15:37:46 +0530

    Create Array2.cpp

---
+++ Array2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int nums[3];
+   
+   nums[0] = 10;
+   nums[1] = 20;
+   nums[2] = 30;
+   
+   for (int i = 0; i < 3; ++i) {
+       cout<<nums[i]<<" ";
+   }
+   
+   return 0;
+}
