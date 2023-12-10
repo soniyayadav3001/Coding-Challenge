34753371072df5ccb9d45b26830980e3b0166dc4
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-12-10 20:02:36 +0530

    Update Array2.cpp

--- Array2.cpp
+++ Array2.cpp
@@ -2,11 +2,9 @@
 using namespace std;
 int main() {
    int nums[3];
-   
    nums[0] = 10;
    nums[1] = 20;
    nums[2] = 30;
-   
    for (int i = 0; i < 3; ++i) {
        cout<<nums[i]<<" ";
    }
